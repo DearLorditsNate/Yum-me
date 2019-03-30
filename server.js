961bd141065dc343987e861bd98aa0477b6243a7
Nate Micinski <nmicinski@gmail.com>
2019-03-30 10:02:14 -0700

    basic architecture

---
+++ server.js
@@ -0,0 +1,25 @@
+// Dependencies
+// Express
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Handlebars
+var exphbs = require("express-handlebars");
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
+);
+app.set("view engine", "handlebars");
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Start server
+app.listen(PORT, function() {
+    console.log("Listening on port: " + PORT);
+});
