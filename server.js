ee58231e5ca99451ec8b9621a3a3cd97d9635994
MichaelWilkens <mwilkens12@gmail.com>
2019-03-30 12:27:23 -0700

    add some BS

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
+
+require("./routes/htmlRoutes.js")(app);
+
+
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
