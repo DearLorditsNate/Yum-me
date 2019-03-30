819190a3cbdb8778b1b382c18a58c0f9d627ace9
Nate Micinski <nmicinski@gmail.com>
2019-03-30 11:17:19 -0700

    basic api call works

---
+++ routes/apiCalls.js
@@ -0,0 +1,64 @@
+require("dotenv").config();
+var keys = require("../keys.js");
+var axios = require("axios");
+
+var proteinSearch = "steak";
+var vegetableSearch = "brocolli";
+var healthOptionsArray;
+
+axios.get("https://api.edamam.com/search?app_id=d6442f57&app_key=69127160173cd4569256538b60dfbc9c&q=steak").then(function(response) {
+    for (var i = 0; i < 5; i++) {
+        console.log(response.data.hits[i]);
+    }
+    console.log(keys.edamam.id);
+});
+
+// $.ajax({
+//     url: "https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?app_id=" + keys.edamam.id + "&app_key=" + keys.edamam.key + "&q=" + proteinSearch + "," + vegetableSearch + healthOptionsArray,
+//     method: 'GET'
+// }).then(function (response) {
+
+//     console.log(response);
+
+    // $('.ingredient-start').css('display', 'block');
+
+    // var random = Math.floor((Math.random() * 8));
+    // for (var i = 0; i < 3; i++) {
+
+    //     var thisHit = response.hits[random].recipe;
+
+    //     //build each recipe
+    //     $('#card-' + i + '-img').attr('src', thisHit.image);
+    //     $('#card-' + i + '-title').text(thisHit.label);
+    //     $('#ul-' + i).append($('<li>').text('Serving size: ' + thisHit.yield));
+    //     $('#recipe-url-' + i).text("See Full Recipe").attr('href', thisHit.url).attr('target', '_blank');
+
+    //     //add link, title and photo to 'save recipe' button
+    //     $('#save-recipe-' + i).attr('link', thisHit.url);
+    //     $('#save-recipe-' + i).attr('title', thisHit.label);
+    //     $('#save-recipe-' + i).attr('photo', thisHit.image);
+
+    //     //Populate ingredients popup card
+    //     ingredients(response);
+    //     $('#popup-title-' + i).text(thisHit.label);
+    //     for (var j = 0; j < thisHit.ingredients.length; j++) {
+    //         $('#popup-ingredients-' + i).append($('<li class="ingredient">').text(thisHit.ingredients[j].text));
+    //     };
+
+    //     random++;
+
+    //     //add in dietlabels
+    //     for (var j = 0; j < thisHit.dietLabels.length; j++) {
+    //         $('#ul-' + i).append($('<li>').text(thisHit.dietLabels[j]));
+    //     };
+
+    //     //add in healthlabels
+    //     for (var j = 0; j < 2; j++) {
+    //         $('#ul-' + i).append($('<li>').text(thisHit.healthLabels[j]));
+    //     };
+    //     //reset input values
+    //     $('#protein-search').val('');
+    //     $('#vegetable-search').val('');
+
+    // };
+// });
