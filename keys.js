c86acab737ccf299279549bcb50b2859f7ad244d
Nate Micinski <nmicinski@gmail.com>
2019-03-30 12:14:27 -0700

    api sends results to frontend

--- keys.js
+++ keys.js
@@ -1,3 +1,8 @@
 exports.mysql = {
-    password: process.env.MYSQL_PASS,
-};
+    password: process.env.MYSQL_PASS
+}
+
+exports.edamam = {
+    key: process.env.EDAMAM_KEY,
+    id: process.env.EDAMAM_APP_ID
+}
