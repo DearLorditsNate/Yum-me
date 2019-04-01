b82acf29ce8b43fadb15369446182fd31e46200e
Nate Micinski <nmicinski@gmail.com>
2019-03-31 21:08:59 -0700

    search now returns json from api to frontend

--- public/js/renderResults.js
+++ public/js/renderResults.js
@@ -5,18 +5,22 @@ $(document).ready(function() {
         var recipeSearch = $('#recipe-search').val().trim();
         console.log(recipeSearch);
 
-        // $.post("/api/search", recipeSearch).then(function(response) {
-        //     // console.log(response);
-        // });
-
         $.ajax({
             url: "/api/search",
-            type: "POST",
+            type: "GET",
             data: {
                 search: recipeSearch
             }
         }).then(function(response) {
             console.log(response);
+            // Render results to the DOM
+            for (var i = 0; i < response.meals.length; i++) {
+                console.log(response.meals[i]);
+                // Name
+                console.log(response.meals[i].strMeal);
+                // Image
+                console.log(response.meals[i].strMealThumb);
+            }
         });
     });
 });
--- routes/apiCalls.js
+++ routes/apiCalls.js
@@ -3,23 +3,14 @@ var keys = require("../keys.js");
 var axios = require("axios");
 
 module.exports = function (app) {
-    app.post("/api/search", function (req, res) {
-        console.log("test");
-        console.log(req.body);
-        // console.log(res);
-        // axios
-        //     .get(
-        //         "https://www.themealdb.com/api/json/v1/1/search.php?s=chicken"
-        //     )
-        //     .then(function (response) {
-        //         //   for (var i = 0; i < 5; i++) {
-        //         //       console.log(response);
-        //         //     //   console.log(response.meals);
-        //         //     //   res.json(response.data.hits);
-        //         //   }
-        //         console.log(response);
-        //         res.json(response.data);
-        //     });
-        res.send("response from the server");
+    app.get("/api/search", function (req, res) {
+        console.log(req.query.search);
+        axios
+            .get(
+                "https://www.themealdb.com/api/json/v1/1/search.php?s=" + req.query.search
+            )
+            .then(function (response) {
+                res.json(response.data);
+            });
     });
 }
