865ca7333ad6b93b798c9ef6624c2fa7c7ef6edc
Nate Micinski <nmicinski@gmail.com>
2019-04-01 21:43:54 -0700

    create get route for retreiving saved recipes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,24 @@
 var db = require('../models')
 
 module.exports = function(app){
-    app.post('/api', function(req, res){
+    // Add recipe
+    app.post('/api/save', function(req, res) {
+        console.log(req.body);
         db.Recipe.create({
             name: req.body.name,
             image: req.body.image,
             instructions: req.body.instructions,
             ingredientName: req.body.ingredientName,
             ingredientMeasure: req.body.ingredientMeasure
-        }).then(function(response){res.json(response)})
-    })
+        }).then(function(response) {
+            res.json(response);
+        });
+    });
+
+    // Get saved recipes
+    app.get('/api/save', function(req, res) {
+        db.Recipe.findAll({}).then(function(recipes) {
+            res.json(recipes);
+        });
+    });
 }
