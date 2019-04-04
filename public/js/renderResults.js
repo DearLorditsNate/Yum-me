c005d3418882f42807659ed03482a14a7a65bc0f
Nate Micinski <nmicinski@gmail.com>
2019-04-03 19:24:29 -0700

    delete route works

--- public/js/renderResults.js
+++ public/js/renderResults.js
@@ -22,4 +22,10 @@ $(document).ready(function () {
             console.log(response);
         });
     });
+
+    $('.delete-fave').on('click', function(event) {
+        event.preventDefault();
+        var id = $(this).attr("data-id");
+        window.location = '/api/delete?id=' + id;
+    });
 });
