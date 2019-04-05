4d47d5729dfddd0c7aba2a50fdb5275d01bb0582
Nate Micinski <nmicinski@gmail.com>
2019-04-04 21:35:56 -0700

    ingredients pretty now

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,6 +17,32 @@ module.exports = function(app){
     // Get saved recipes
     app.get('/profile/favorites', function(req, res) {
         db.Recipe.findAll({}).then(function(recipes) {
+            for (var i = 0; i < recipes.length; i++) {
+                var measurementArr = [];
+                var measurementString = recipes[i].ingredientMeasure;
+
+                var itemArr = [];
+                var itemString = recipes[i].ingredientName;
+
+                measurementString = measurementString.split(',');
+                for (var j = 0; j < measurementString.length; j++) {
+                    if (measurementString[j] !== " ") {
+                        measurementArr.push(measurementString[j].trim());
+                    }
+                }
+
+                itemString = itemString.split(',');
+                for (var k = 0; k < itemString.length; k++) {
+                    if (itemString[k] !== " ") {
+                        itemArr.push(itemString[k]);
+                    }
+                }
+
+                for (var l = 0; l < measurementArr.length; l++) {
+                    measurementArr[l] = measurementArr[l] + itemArr[l];
+                }
+                recipes[i].ingredients = measurementArr;
+            }
             res.render('favorites', {recipes: recipes});
         });
     });
